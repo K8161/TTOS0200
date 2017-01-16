c0dc422709508669a058638326bf3170c4d4465d
K8161 <K8161@student.jamk.fi>
2017-01-16 19:26:37 +0200

    Teht7

---
+++ vko3/vko3/TV.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    class TV
+    {
+        //properties
+        public bool OnkoPaalla { get; set; }
+        int kanava;
+        public int Kanava {
+            get { return kanava; }
+            set
+            {
+                kanava = value;
+                if (kanava < 1 || kanava > 30)
+                {
+                    äänenvoimakkuus = 3;
+                }
+            }
+        }
+        int äänenvoimakkuus;
+        public int Äänenvoimakkuus
+        {
+            get { return äänenvoimakkuus; }
+            set
+            {
+                äänenvoimakkuus = value;
+                if (äänenvoimakkuus < 0 || äänenvoimakkuus > 100)
+                {
+                    äänenvoimakkuus = 50;
+                }
+            }
+        }
+        //methods
+        public void LisääÄäntä()
+        {
+            äänenvoimakkuus += 10;
+        }
+        public void LaskeÄäntä()
+        {
+            äänenvoimakkuus -= 10;
+        }
+
+        public void VaihdaKanavaYlös()
+        {
+            kanava += 1;
+        }
+        public void VaihdaKanavaAlas()
+        {
+            äänenvoimakkuus -= 1;
+        }
+    }
+}
