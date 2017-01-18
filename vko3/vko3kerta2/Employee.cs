5457d00d02c350a5e6dd250bebb9d13727a963f4
K8161 <K8161@student.jamk.fi>
2017-01-18 15:41:07 +0200

    Teht3

---
+++ vko3/vko3kerta2/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko3kerta2
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Profession { get; set; }
+        public string Salary { get; set; }
+
+        // default constructor
+        public Employee()
+        {
+        }
+
+        // constructor takes person firstname and lastname as a parameter
+        public Employee(string fullName, string profession, string salary)
+        {
+            Name = fullName;
+            Profession = profession;
+            Salary = salary;
+        }
+
+        // just one method what Person can do
+        public void EmployeeMethod()
+        {
+            Console.WriteLine("This method belongs to Employee!");
+        }
+
+        // return Person data as a one string, override Object class ToString()-method
+        public override string ToString()
+        {
+            return "Name: " + Name + " Profession: " + Profession + " Salary: " + Salary;
+        }
+    }
+}
--- vko3/vko3kerta2/Program.cs
+++ vko3/vko3kerta2/Program.cs
@@ -11,7 +11,8 @@ namespace vko3kerta2
         static void Main(string[] args)
         {
             //UseElevator();
-            UseAmplifier();
+            //UseAmplifier();
+            Staff();
         }
 
         static void UseElevator()
@@ -50,6 +51,30 @@ namespace vko3kerta2
                 Console.WriteLine("How much volume is wanted? ");
                 amplifier.Volume = Convert.ToInt32(Console.ReadLine());
             }
+        }
+
+        static void Staff()
+        {
+            // create a one Person object
+            Employee employee = new Employee();
+            employee.Name = "Kirsi Kernel";
+            employee.Profession = "Kahvinkeittäjä";
+            employee.Salary = "1624";
+            Console.WriteLine(employee.ToString());
+
+            // create a one Teacher Object
+            Boss boss = new Boss();
+            boss.Name = "Teppo Tulppu";
+            boss.Profession = "Iso Pamppu";
+            boss.Salary = "5000";
+            boss.Car = "Porsche";
+            boss.Bonus = 666;
+            Console.WriteLine(boss.ToString());
+
+
+            // do something methods
+            employee.EmployeeMethod(); // This method belongs to Person!
+            boss.BossMethod(); // This method belongs to Person! (derived class can use it!)
         }
     }
 }
