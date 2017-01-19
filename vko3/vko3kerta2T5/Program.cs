1a71128aaefaf2d51ae2f7f18d175adf50697e6c
K8161 <K8161@student.jamk.fi>
2017-01-19 13:14:21 +0200

    T5 started

---
+++ vko3/vko3kerta2T5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko3kerta2T5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseRadio();
+        }
+
+        static void UseAmplifier()
+        {
+            //creating amplifier object
+            Amplifier amplifier = new Amplifier();
+            //volume is set to 20 by default
+            amplifier.Volume = 20;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                //show Volume and ask how much Volume is wanted
+                Console.WriteLine(amplifier.ToString());
+                Console.WriteLine("How much volume is wanted? ");
+                amplifier.Volume = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ vko3/vko3kerta2T5/Radio.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko3kerta2T5
+{
+    class Radio
+    {
+        private int volume, frequency;
+        private int isVolumeValid;
+        private int isFrequencyValid;
+        private const int maxVol = 100;
+        private const int minVol = 0;
+        private const float maxFreq = 26000;
+        private const float minFreq = 2000;
+        public int Volume
+        {
+            get { return volume; }
+            set
+            {
+                volume = value;
+
+                if (volume < minVol)
+                {
+                    isVolumeValid = 1;
+                    volume = 0;
+                }
+
+                else if (volume > maxVol)
+                {
+                    isVolumeValid = 2;
+                    volume = 100;
+                }
+
+                else
+                {
+                    isVolumeValid = 3;
+                    volume = value;
+                }
+            }
+        }
+
+
+        public int IsVolumeValid
+        {
+            get { return isVolumeValid; }
+        }
+        public string VolumeMessage()
+        {
+            if (isVolumeValid == 1)
+                return "Current volume " + Volume;
+            else
+                return "Volume only available from range 0-100";
+        }
+
+        public int Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                frequency = value;
+
+                if (frequency < minFreq)
+                {
+                    isFrequencyValid = false;
+                    volume = 0;
+                }
+
+                else if (frequency > maxFreq)
+                {
+                    isFrequencyValid = false;
+                    volume = 100;
+                }
+
+                else
+                {
+                    isFrequencyValid = true;
+                    volume = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            if (IsVolumeValid)
+            {
+                return VolumeMessage();
+            }
+            else
+            {
+                return VolumeMessage();
+            }
+        }
+    }
+}
