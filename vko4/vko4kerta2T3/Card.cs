c4afa6100b42355842516cab7054789465e2250f
K8161 <K8161@student.jamk.fi>
2017-01-25 15:55:11 +0200

    T3 classes done

---
+++ vko4/vko4kerta2T3/Card.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko4kerta2T3
+{
+    abstract class Card
+    {
+        public int Number { get; set; }
+        public virtual string Suit { get; set; }
+
+        public Card()
+        { }
+
+        public Card(int number)
+        {
+            Number = number;
+        }
+
+        public abstract void ShowCard();
+    }
+    //Hearts class
+    class Hearts : Card
+    {
+        public override string Suit
+        {
+            get
+            {
+                return "Hearts";
+            }
+
+            set
+            {
+                Suit = value;
+            }
+        }
+
+        public override void ShowCard()
+        {
+            Console.WriteLine(Number + " of " + Suit);
+        }
+
+        //Diamonds class
+        class Diamonds : Card
+        {
+            public override string Suit
+            {
+                get
+                {
+                    return "Hearts";
+                }
+
+                set
+                {
+                    Suit = value;
+                }
+            }
+
+            public override void ShowCard()
+            {
+                Console.WriteLine(Number + " of " + Suit);
+            }
+
+            //Clubs class
+            class Clubs : Card
+            {
+                public override string Suit
+                {
+                    get
+                    {
+                        return "Hearts";
+                    }
+
+                    set
+                    {
+                        Suit = value;
+                    }
+                }
+
+                public override void ShowCard()
+                {
+                    Console.WriteLine(Number + " of " + Suit);
+                }
+
+                //Spades class
+                class Spades : Card
+                {
+                    public override string Suit
+                    {
+                        get
+                        {
+                            return "Hearts";
+                        }
+
+                        set
+                        {
+                            Suit = value;
+                        }
+                    }
+
+                    public override void ShowCard()
+                    {
+                        Console.WriteLine(Number + " of " + Suit);
+                    }
+                }
+            }
+        }
+    }
+}
+                
+            
