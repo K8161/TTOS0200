f422db96349fbfc775041343664dd7608e43658d
K8161 <K8161@student.jamk.fi>
2017-01-27 08:45:40 +0200

    T4 start

---
+++ vko4/vko4kerta2T4/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko4kerta2T4
+{
+    class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TimeOfBirth { get; set; }
+        public int PlayerNumber { get; set; }
+    }
+}
---
+++ vko4/vko4kerta2T4/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko4kerta2T4
+{
+    class Team
+    {
+        public string TeamName { get; set; }
+        public string HomeCity { get; set; }
+        public string Players { get; set; }
+    }
+}
