766579fecf1e7a7bfa2837367c13655451bad93c
K8161 <K8161@student.jamk.fi>
2017-01-30 13:14:43 +0200

    T2 done

---
+++ vko5/vko5Kerta1T2/FileHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.vko5Kerta1T2
+{
+    class FileHandler
+    {
+        public void ReadFile()
+        {
+            try
+            {
+                /*     string[] lines = System.IO.File.ReadAllLines(@"..\..\test.txt");
+                     foreach (string line in lines)
+                     {
+                         Console.WriteLine(line);
+                     }
+                     Console.WriteLine("Found " + lines.Length + " lines."); */
+
+                var nameCount = new Dictionary<string, int>();
+
+                String[] lines = File.ReadAllLines(@"..\..\test.txt");
+                Console.WriteLine("Found " + lines.Length + " lines.");
+                foreach (String s in File.ReadAllLines(@"..\..\test.txt"))
+                {
+                    if (nameCount.ContainsKey(s))
+                    {
+                        nameCount[s] = nameCount[s] + 1;
+                    }
+                    else
+                    {
+                        nameCount.Add(s, 1);
+                    }
+                }
+
+                // and printing
+                foreach (var pair in nameCount)
+                {
+                    Console.WriteLine("{0} found: {1} times", pair.Key, pair.Value);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+        }
+    }
+}
---
+++ vko5/vko5Kerta1T2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.vko5Kerta1T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            T2();
+        }
+
+        static void T2()
+        {
+            FileHandler test = new FileHandler();
+
+            test.ReadFile();
+        }
+    }
+}
