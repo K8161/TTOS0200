6ffc81647ff1816b8c9821327f598016ef5c1fba
K8161 <K8161@student.jamk.fi>
2017-01-30 12:20:49 +0200

    T1 done

---
+++ vko5/vko5Kerta1T1/IOtesting.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.vko5Kerta1T1
+{
+    class IOtesting
+    {
+        public string Text { get; set; }
+
+        public void SaveText()
+        {
+            StreamWriter outputFile = null;
+
+            try
+            {
+                Console.WriteLine("Welcome to textsaver Pro 2000!");
+                outputFile = new System.IO.StreamWriter(@"SavedText.txt");
+                do
+                {
+                    Console.WriteLine("Give text to save: ");
+                    Text = Console.ReadLine();
+                    outputFile.WriteLine(Text);
+                } while (Text != "exit");
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Some exception happened!");
+                Console.WriteLine(ex.Message); // Access denied.
+            }
+            finally
+            {
+                // check for null because OpenWrite might have failed
+                if (outputFile != null)
+                {
+                    outputFile.Close();
+                }
+            }
+        }
+
+        public void ReadText()
+        {
+            // @"D:\K8161\TTOS0200\vko5\vko5Kerta1T1\SavedText.txt"
+            try
+            {
+            string text = System.IO.File.ReadAllText(@"SavedText.txt");
+        System.Console.WriteLine("Your nonsense printed:\n" + text);
+        } catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found (FileNotFoundException)");
+        }
+      }
+    }
+}
--- vko5/vko5Kerta1T1/Program.cs
+++ vko5/vko5Kerta1T1/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace vko5Kerta1T1
+namespace JAMK.IT.vko5Kerta1T1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ReadingTest();
+        }
+
+        static void ReadingTest()
+        {
+            IOtesting ioTesting = new IOtesting();
+
+            ioTesting.SaveText();
+            ioTesting.ReadText();
         }
     }
 }
