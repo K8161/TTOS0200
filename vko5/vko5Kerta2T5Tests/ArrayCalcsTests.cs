dacf5978f051f9ef926f7e19ffb5e3b54acf30ef
K8161 <K8161@student.jamk.fi>
2017-02-03 10:05:25 +0200

    Tests created

---
+++ vko5/vko5Kerta2T5Tests/ArrayCalcsTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using vko5Kerta2T5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vko5Kerta2T5.Tests
+{
+    [TestClass()]
+    public class ArrayCalcsTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            //arrange
+            ArrayCalcs calc = new ArrayCalcs();
+            double sumAnswer = 0;
+            double[] array = { 1.0, 2.0 };
+            int expected = 3;
+
+            //act
+            int actual = calc.Sum(array);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+            //ja toinen testi Add-metodilla
+            a = 0;
+            b = 1;
+            expected = 1;
+            actual = calc.Add(a, b);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AverageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MinTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MaxTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
